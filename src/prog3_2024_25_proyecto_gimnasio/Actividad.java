4470cfb3508847211a779b42cfbb4e18ab009c67
Illart <illart.d@opendeusto.es>
2024-10-30 18:42:50 +0100

    Creacion Clase Actividad
    Creacion y Layout

---
+++ src/prog3_2024_25_proyecto_gimnasio/Actividad.java
@@ -0,0 +1,21 @@
+package prog3_2024_25_proyecto_gimnasio;
+
+import java.awt.BorderLayout;
+
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Actividad extends JFrame{
+	private static final long serialVersionUID = 1L;
+	
+	public Actividad() {
+
+		JPanel pActividad = new JPanel(new BorderLayout(5, 2));
+		
+		JPanel informacion = new JPanel();
+		informacion.setLayout(new BoxLayout(informacion, BoxLayout.Y_AXIS));
+		
+	
+	}
+}
