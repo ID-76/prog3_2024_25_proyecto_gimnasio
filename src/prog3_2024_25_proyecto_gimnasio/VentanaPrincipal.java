7fefdec8063e380f8e1d2adf56fa096ae52f4e0e
Illart <illart.d@opendeusto.es>
2024-10-30 22:42:56 +0100

    Switch case Ventana Principal

--- src/prog3_2024_25_proyecto_gimnasio/VentanaPrincipal.java
+++ src/prog3_2024_25_proyecto_gimnasio/VentanaPrincipal.java
@@ -1,12 +1,14 @@
 package prog3_2024_25_proyecto_gimnasio;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
+import prog3_2024_25_proyecto_gimnasio.Actividad;
 
 
 
@@ -25,7 +27,7 @@ public class VentanaPrincipal extends JFrame {
 		
 		
 		String[] buttonsText = {
-				"HOME",
+				"MENU",
 				"CLASES",
 				"SALUD",
 				"USUARIO",
@@ -37,8 +39,27 @@ public class VentanaPrincipal extends JFrame {
 			
 			boton.addActionListener( e -> {
 				JButton btnPulsado = (JButton) e.getSource();
-			
 				// ACCION POR IMPLEMENTAR
+				switch(text) {
+				  case "CLASES":
+					Actividad actPanel = new Actividad();
+					principal.add(actPanel.pActividad, BorderLayout.EAST);
+					
+				    break;
+				  case "SALUD":
+				    System.out.println("SALUD");
+					  
+				    break;
+				  case "USUARIO":
+					System.out.println("USUARIO");
+					
+					break;
+				  case "MENU":
+				  default:
+					System.out.println("MENU");
+				    // code block
+				}
+
 			});
 			sidebar.add(boton);
 		}
@@ -48,6 +69,8 @@ public class VentanaPrincipal extends JFrame {
 		setSize(200, 350);
 		add(principal);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setMinimumSize(new Dimension(500, 300));
+        setLocationRelativeTo(null);
 
 		setTitle("HOME");
 		
