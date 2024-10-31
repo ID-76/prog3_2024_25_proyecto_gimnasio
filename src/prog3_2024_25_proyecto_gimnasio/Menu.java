3dda5d213220fc7e44fb930d6afcc3f79b23abfe
Illart <illart.d@opendeusto.es>
2024-10-31 12:48:06 +0100

    Creacion de Clases
    Creacion de las clases Menu, Usuario y Salud. Borrado un archivo HolaMundo de prueba.

---
+++ src/prog3_2024_25_proyecto_gimnasio/Menu.java
@@ -0,0 +1,10 @@
+package prog3_2024_25_proyecto_gimnasio;
+
+import javax.swing.JPanel;
+
+public class Menu extends JPanel{
+	
+	public Menu() {
+		
+	}
+}
---
+++ src/prog3_2024_25_proyecto_gimnasio/Usuario.java
@@ -0,0 +1,9 @@
+package prog3_2024_25_proyecto_gimnasio;
+
+import javax.swing.JPanel;
+
+public class Usuario extends JPanel{
+	public Usuario() {
+		
+	}
+}
