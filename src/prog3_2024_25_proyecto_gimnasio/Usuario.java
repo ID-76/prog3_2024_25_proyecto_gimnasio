1c8f1ec63a979c603a009dd11f0cf5f47f9d1729
AsierGomez09 <asier.gomez@opendeusto.es>
2024-10-31 19:23:32 +0100

    Actualización Usuario

--- src/prog3_2024_25_proyecto_gimnasio/Usuario.java
+++ src/prog3_2024_25_proyecto_gimnasio/Usuario.java
@@ -1,9 +1,23 @@
 package prog3_2024_25_proyecto_gimnasio;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Usuario extends JPanel{
 	public Usuario() {
+		setLayout(new BorderLayout());
+		JPanel encabezados = new JPanel(new GridLayout(6,2,4,4));
+		add(encabezados, BorderLayout.WEST);
+		String[] encabezados_text = {"Nombre", "Apellido", "DNI", "Telefono", "Edad", "Sexo"};
+		for (String encabezado : encabezados_text) {
+			encabezados.add(new JLabel(encabezado));
+			encabezados.add(new JTextField());
+		}
+		
 		
 	}
 }
