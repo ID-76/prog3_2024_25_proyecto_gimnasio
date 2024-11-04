b729f32ca705699808b9f00d7d671fc52c36dc0c
AsierGomez09 <asier.gomez@opendeusto.es>
2024-11-04 11:41:53 +0100

    Cambios en el inicio

--- src/prog3_2024_25_proyecto_gimnasio/InicioSesion.java
+++ src/prog3_2024_25_proyecto_gimnasio/InicioSesion.java
@@ -1,13 +1,12 @@
-	package prog3_2024_25_proyecto_gimnasio;
-	
-	import java.awt.BorderLayout;
+package prog3_2024_25_proyecto_gimnasio;
+
+import java.awt.Color;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-	import prog3_2024_25_proyecto_gimnasio.Usuario.Sexo;
 	
 	public class InicioSesion extends JPanel {
 		private static final long serialVersionUID = 1L;
@@ -56,6 +55,7 @@ import javax.swing.JPanel;
 			add(icono);;
 			add(matricula);
 			add(ini_ses);
+			setBackground(Color.black);
 		}
 		
 		public Usuario getUsuario() {
